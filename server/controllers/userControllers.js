08ea3824947842478837ad2dd3fe8576fbbecc6a
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-04-16 12:21:08 +0530

    headers token fix

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -93,17 +93,21 @@ exports.login = async (req, res, next) => {
         expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
         httpOnly: true,
       };
-      res.cookie("token", token, options).status(200).json({
+      res
+        .cookie("token", token, options)
+        .status(200)
+        .json({
         success: true,
         user,
         token,
       });
+      return next();
     }
     res.status(400).send("Incorrect credincial ");
   } catch (error) {
     console.log(error.message);
   }
-  next(); 
+  return next(error);
 };
 
 
