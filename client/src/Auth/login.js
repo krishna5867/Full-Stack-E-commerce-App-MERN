431386f556bf96538f83646823748d2f07765ae8
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-04-19 12:37:08 +0530

    headers

--- client/src/Auth/login.js
+++ client/src/Auth/login.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../Redux/authSlice';
 import { useNavigate } from 'react-router-dom';
-// import axios from 'axios';
+import axios from 'axios';
 import { Form, Container, Card, CardBody, Button, Input, Label } from 'reactstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
@@ -17,55 +17,53 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const data = await fetch("/login", {
-                method: "POST",
+            const response = await axios.post("/login", {
+                email,
+                password
+            }, {
                 headers: {
                     "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    email, password
-                })
+                }
             });
-            const res = await data.json();
+            const res = response.data;
             toast.success("Login Success");
             navigate("/");
-            // window.location.reload()
             localStorage.setItem("user", JSON.stringify(res.user));
-            // console.log(res,'31');
             if (res.status === 200) {
                 dispatch(login(res.user));
             }
-} catch (error) {
-    console.error(error);
-    toast.error("Invalid credentials");
-}
+        } catch (error) {
+            console.error(error);
+            toast.error("Invalid credentials");
+        }
     };
 
-return (
-    <>
-        <ToastContainer />
-        <Container className='mt-5' style={{ width: '30rem' }}>
-            <Card className="border m-2 p-4 shadow-lg bg-white rounded">
-                <CardBody>
-                    <h2>Login Form</h2>
-                    <Form onSubmit={handleLogin} className='mt-5'>
-                        <div className='d-flex flex-column justify-content-start align-items-start'>
-                            <Label htmlFor="email">Email</Label>
-                            <Input type="email" placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <div className='d-flex flex-column justify-content-start align-items-start mt-3'>
-                            <Label htmlFor="password">Password</Label>
-                            <Input type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <Button className='bg-primary mt-4 mb-3 w-100' type="submit">Login</Button>
-                        <p>Don't have Account <Link to="/signup">Create New Account</Link></p>
-                        <p><Link to='/forgetPassword'>Forget Password </Link></p>
-                    </Form>
-                </CardBody>
-            </Card>
-        </Container>
-    </>
-)
+
+    return (
+        <>
+            <ToastContainer />
+            <Container className='mt-5' style={{ width: '30rem' }}>
+                <Card className="border m-2 p-4 shadow-lg bg-white rounded">
+                    <CardBody>
+                        <h2>Login Form</h2>
+                        <Form onSubmit={handleLogin} className='mt-5'>
+                            <div className='d-flex flex-column justify-content-start align-items-start'>
+                                <Label htmlFor="email">Email</Label>
+                                <Input type="email" placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className='d-flex flex-column justify-content-start align-items-start mt-3'>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+                            <Button className='bg-primary mt-4 mb-3 w-100' type="submit">Login</Button>
+                            <p>Don't have Account <Link to="/signup">Create New Account</Link></p>
+                            <p><Link to='/forgetPassword'>Forget Password </Link></p>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Container>
+        </>
+    )
 
 }
 
